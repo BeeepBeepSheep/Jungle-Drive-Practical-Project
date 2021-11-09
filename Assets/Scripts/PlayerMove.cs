ee039e5a0bc0ac1c2c19de5d1658a8cb3189b322
BeeepBeepSheep <jacoblueke@gmail.com>
2021-11-09 13:25:08 +0000

    steering

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,22 +4,83 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float currantSpeed = 10f;
+    public float currantSpeed = 25f;
+    public float currantHorizontalSpeed = 25f;
+    public float baseHorizontalSpeed = 25f;
     public Rigidbody rb;
-    float steeringInput;
-
-    public float maxSteer = 25f;
-    public float minSteer = -25f;
+    float horizontalInput;
 
     public GameObject car;
 
-    public float steeringSpeed = 3f;
+    public Animator carAnim;
 
+    public int turnStateInt = 1;
+    //if 0 left, if 1 forward, if 2 right;
+    void Start()
+    {
+        currantHorizontalSpeed = baseHorizontalSpeed;
+    }
+    void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * currantSpeed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * currantHorizontalSpeed * Time.fixedDeltaTime;
+        
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
     void Update()
     {
-        rb.velocity = car.transform.forward * (currantSpeed);
+        horizontalInput = Input.GetAxis("Horizontal");
 
-        steeringInput += Input.GetAxis("Mouse X") * steeringSpeed;
-        car.transform.rotation = Quaternion.Euler(transform.rotation.y, steeringInput, 0f);
+        carAnim.SetInteger("TurnStateInt", turnStateInt);
+        InputManagment();
+
+        //fix horizontalm ove
+        if (turnStateInt == 2)
+        {
+            currantHorizontalSpeed = 0;
+        }
+        else
+        {
+            currantHorizontalSpeed = baseHorizontalSpeed;
+        }
+    }
+    void InputManagment()
+    {
+        //normalize
+        if (Input.GetKeyUp("a"))
+        {
+            turnStateInt = 2;
+        }
+        if (Input.GetKeyUp("d"))
+        {
+            turnStateInt = 2;
+        }
+        if (Input.GetKey("a") && Input.GetKey("b"))
+        {
+            turnStateInt = 2;
+        }
+        //left
+        if (Input.GetKey("a"))
+        {
+            turnStateInt = 0;
+            if (Input.GetKey("d"))
+            {
+                turnStateInt = 2;
+            }
+        }
+
+        //right
+        if (Input.GetKey("d"))
+        {
+            turnStateInt = 3;
+            if (Input.GetKey("a"))
+            {
+                turnStateInt = 2;
+            }
+        }
     }
 }
+//mouse steer
+
+//steeringInput += Input.GetAxis("Mouse X") * steeringSpeed;
+//car.transform.rotation = Quaternion.Euler(transform.rotation.y, steeringInput, 0f);
