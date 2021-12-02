9041f8fd42224e40f44ee83838de0a0ecd420073
BeeepBeepSheep <jacoblueke@gmail.com>
2021-12-02 20:04:42 +0000

    all obsticals

--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -6,14 +6,16 @@ public class GroundTile : MonoBehaviour
 {
     public GroundSpawner groundSpawnerScript;
 
-    public GameObject[] obsticles;
     public GameObject obsticleToSpawn;
     public int obsticleIndex;
 
-    public float maxTreeSize = 4f;
-    public float minTreeSize = 1.5f;
+    public float minTallTreeSize = 1.5f;
+    public float maxTallTreeSize = 4f;
+    public float minBasicTreeSize = 1f;
+    public float maxBasicTreeSize = 3f;
+    public float minHugeTreeSize = 1f;
+    public float maxHugeTreeSize = 1.5f;
 
-    public Transform[] spawnPoints;
     public GameObject TileHolder;
     public GameObject player;
 
@@ -22,6 +24,12 @@ public class GroundTile : MonoBehaviour
     public int obsticlesToSpawnAmmount;
     public int minObsticles = 2;
     public int maxObsticles = 5;
+
+    int rockColourIndex;
+
+    public GameObject[] obsticles;
+    public Material[] rockColours;
+    public Transform[] spawnPoints;
 
     void Start()
     {
@@ -34,9 +42,8 @@ public class GroundTile : MonoBehaviour
         //player = GameObject.FindGameObjectWithTag("Player");
 
         //obsticlesToSpawnAmmount = Random.Range(minObsticles, maxObsticles);
-        obsticlesToSpawnAmmount = maxObsticles;
 
-        SpawnObsticles(obsticlesToSpawnAmmount);
+        SpawnObsticles(/*obsticlesToSpawnAmmount*/ 1);
     }
 
     public void MyOnTriggerEnter(Collider col)
@@ -51,7 +58,6 @@ public class GroundTile : MonoBehaviour
 
     void SpawnObsticles(int ammountToSpawn)
     {
-        //Debug.Log("test");
         for (int i = minObsticles; i < ammountToSpawn; i++)
         {
             obsticleIndex = Random.Range(0, obsticles.Length);
@@ -59,18 +65,61 @@ public class GroundTile : MonoBehaviour
             GameObject myObsticle = Instantiate(obsticleToSpawn, spawnPoints[Random.Range(0, spawnPoints.Length)].position, obsticleToSpawn.transform.rotation);
 
             myObsticle.transform.parent = transform;
+            // random y rotation
+            int randdonYRot = Random.Range(0, 360);
+            myObsticle.transform.Rotate(Vector3.down * randdonYRot);
 
-            // random size
-            if(myObsticle.tag == "TreeTall")
+            //// trees
+            //if (myObsticle.tag != "Tree HugeMini" && myObsticle.tag == "Rock Circle")
+            //{
+            //    // random y rotation
+            //    int randdonYRot = Random.Range(0, 360);
+            //    myObsticle.transform.Rotate(Vector3.down * randdonYRot);
+
+            //    //size
+            //    if (myObsticle.tag == "TreeTall")
+            //    {
+            //        float currantTreeSize = Random.Range(minTallTreeSize, maxTallTreeSize);
+            //        myObsticle.transform.localScale = new Vector3(currantTreeSize, currantTreeSize, currantTreeSize);
+            //    }
+
+            //    if (myObsticle.tag == "Tree Basic")
+            //    {
+            //        float currantTreeSize = Random.Range(minBasicTreeSize, maxBasicTreeSize);
+            //        myObsticle.transform.localScale = new Vector3(currantTreeSize, currantTreeSize, currantTreeSize);
+            //    }
+
+            //    if (myObsticle.tag == "Tree Huge")
+            //    {
+            //        float currantTreeSize = Random.Range(minHugeTreeSize, maxHugeTreeSize);
+            //        myObsticle.transform.localScale = new Vector3(currantTreeSize, currantTreeSize, currantTreeSize);
+            //    }
+            //}
+
+            ////duckable obsticles 
+            //if (myObsticle.tag == "Tree Huge Mini" || myObsticle.tag == "Rock Circle")
+            //{
+            //    int index = Random.Range(0, 1);
+            //    int yRotation;
+            //    if (index == 0)
+            //    {
+            //        yRotation = 0;
+            //    }
+            //    else
+            //    {
+            //        yRotation = 180;
+            //    }
+            //    myObsticle.transform.Rotate(Vector3.down * yRotation);
+            //}
+
+            if (myObsticle.tag == "Rock Circle" || myObsticle.tag == "Rock")
             {
-                float currantTreeSize = Random.Range(minTreeSize, maxTreeSize);
-                myObsticle.transform.localScale = new Vector3(currantTreeSize, currantTreeSize, currantTreeSize);
 
-                // random y rotation
-                int randdonYRot = Random.Range(0, 360);
-                myObsticle.transform.Rotate(Vector3.down * randdonYRot);
+                rockColourIndex = Random.Range(0, rockColours.Length);
+                Material selectedColour = rockColours[rockColourIndex];
+
+                myObsticle.transform.GetChild(0).GetComponent<MeshRenderer>().material = selectedColour;
             }
-
         }
     }
 }
